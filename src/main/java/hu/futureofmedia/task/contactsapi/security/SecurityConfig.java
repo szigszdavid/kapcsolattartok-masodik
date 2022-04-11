4e2c863d5297736a62e85dbfaa00c37eef4852cb
szigszdavid <szigetkozi.szabo.david@gmail.com>
2022-04-11 10:01:34 +0200

    Home office 10:00 commit, authorization added without priviligies

--- src/main/java/hu/futureofmedia/task/contactsapi/security/SecurityConfig.java
+++ src/main/java/hu/futureofmedia/task/contactsapi/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package hu.futureofmedia.task.contactsapi.security;
 
 import hu.futureofmedia.task.contactsapi.filter.CustomAuthenticationFilter;
+import hu.futureofmedia.task.contactsapi.filter.CustomAuthorizationFilter;
 import hu.futureofmedia.task.contactsapi.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,6 +22,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -54,13 +56,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
         http.authorizeRequests().antMatchers("/login/**").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.GET, "/contacts/**").hasAnyAuthority("USER");
         http.authorizeRequests().antMatchers(HttpMethod.POST, "/contacts/**").hasAnyAuthority("ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
-        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+        http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
